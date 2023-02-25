17b7f1b87f0dd5a73013b733571fd8d6473f7f66
Florence NGUYEN <62519443+thaolynguyen@users.noreply.github.com>
2023-02-25 09:46:19 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import sys
 
 app = Flask(__name__)
 
-openai.api_key = os.getenv("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")
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 @app.route('/')  # this is the home page route
 def hello_world(
