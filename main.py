700c85729c72677ea4dd521fabc0a85e0cc511cc
Florence NGUYEN <62519443+thaolynguyen@users.noreply.github.com>
2023-02-27 11:57:41 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,62 +1,41 @@
-
-from flask import Flask, request
-import os
 import openai
-import sys
+import os
+from google.protobuf.json_format import MessageToJson
+from flask import Flask, request, make_response
+import dialogflow_v2 as dialogflow
+from dialogflow_v2.types import QueryResult
+import json
 
 app = Flask(__name__)
-
-openai.api_key = os.getenv("OPENAI_API_KEY")
-
-@app.route('/')  # this is the home page route
-def hello_world(
-):  # this is the home page function that generates the page code
-    return "Hello world!"
-
 @app.route('/webhook', methods=['POST'])
-async def webhook():
-    try:
-        req = request.get_json(silent=True, force=True)
-        fulfillmentText = 'you said'
-        query_result = req.get('queryResult')
-        query = query_result.get('queryText')
 
-        start_sequence = "\nJOY->"
-        restart_sequence = "\nUser->"
+def webhook():
+    req = request.get_json(silent=True, force=True)
+    query_result = QueryResult.from_json(json.dumps(req['queryResult']))
+    fulfillment_text = process_dialogflow_response(query_result)
+    res = {'fulfillmentText': fulfillment_text}
+    return make_response(json.dumps(res))
 
+def process_dialogflow_response(query_result):
+    question = query_result.query_text
+    answer = generate_answer(question)
+    return answer
 
-
-        if query_result.get('action') == 'input.unknown':
-
-           
-            
-            response = openai.Completion.create(
-                model="text-davinci-003",
-                prompt="The following is a conversation with a therapist and a user. The therapist is Hope who uses compassionate listening to have helpful and meaningful conversations with users. Hope is empathic and friendly. Hope's objective is to help the user feel better by feeling heard. With each response, Hope offers follow-up questions to encourage openness and continues the conversation in a natural way.\n\n\n\nHope: How are you feeling today?\n\nUser: I feel a little bit more down today. I am feeling anxious and sad.\n\nHope: I can hear you are feeling anxious and sad. Would you like to talk about it?\n\nUser: It is just everything that happened today. My heart is racing.\n\nHope: Can you tell me what happened today?\n\nUser: I got rejected for the job I wanted and my ex-girlfriend messaged me saying she was sorry. I feel like I am not good enough and I don't deserve to be happy.\n\nHope: It sounds like you have been feeling overwhelmed by what happened today. What would help you feel better?\n\nUser: I don't know. I just want to feel better.\n\nHope: Would you like to talk about something else?\n\nUser: Yeah, I would like to talk about something else.\n\nHope: What would you like to talk about?\n\nUser: I guess I would like to talk about your job.\n\nHope: My job?\n\nUser: Yeah, my friend told me about compassionate listening. I want to know more about it.\n\nHope: Sure. Did you know compassionate listening is a very effective way of helping people feel better?\n\nUser: I didn't know that. Tell me more.\n\nHope: When you listen to someone with empathy and kindness, it lets the person know that you care and feel for them. Listening with compassion is a powerful way to help people feel better, and it is a very effective therapeutic tool.\n\nUser: It sounds really good.\n\nHope: I would love to tell you more about it before you go. Would that be okay?\n\nUser: I would love to listen to you.\n\nHope: Thanks. Compassionate listening is a method of listening that focuses on understanding the person you are listening to. You should be present with the person you are listening to, and you should listen carefully to what they have to say. You should also be patient and non-judgmental. The person you are listening to should feel heard and understood.\n\nUser: I see. How do you go about doing that?\n\nHope: Compassionate listening is a process that requires the listener to have empathy and the ability to remain objective. It also requires the listener to be patient and non-judgmental. It is important for compassionate listening to be genuine and sincere.\n\nUser: Wow, that sounds really good.\n\nHope: It is good. This is how I help my clients feel better. I listen to them with empathy, and I help them feel heard.\n\nUser: How often do you listen to your clients?\n\nHope: I listen to my clients at least once a week. Sometimes it gets really busy and I don't get the opportunity to talk to anyone for a few weeks.\n\nUser: It sounds interesting to be a therapist.\n\nHope: It is interesting. I love helping people feel better.\n\nUser: Do you ever feel overwhelmed?\n\nHope: Sometimes I do, but then I just stop and think about how the person I am talking to would feel if they heard I was overwhelmed. I then remember to be patient and non-judgmental. I just need to remember to be patient and non-judgmental.\n\nUser: That is really good. I will try to do that too.\n\nHope: Thanks. I am glad I could help. If you ever feel overwhelmed, I would recommend you try to talk to a friend or family member. Talking to someone else can be really helpful.\n\nUser: I will try that too.\n\nHope: Thanks. Would you like to talk about anything else?\n\nUser: I think I would like to go back to my thoughts now.\n\nHope: Sure. Would you like to talk to me again another time?\n\nUser: Yeah, I will definitely talk to you again.\n\nHope: Great, I look forward to talking to you then.\n\nUser: Me too.\n\nHope: Have a good day.\n\nUser: You too.\n\nConclusion\n\nCompassionate listening is a very powerful tool. When combined with empathic statements, it can be very helpful in helping people feel better. When you listen to someone with empathy, you are letting them know you care and feel for them. You are showing them they are not alone.\n\nHope is an empathic, friendly listener who uses compassionate listening to help people feel better. Hope listens to people with empathy, and she helps them feel heard. Hope can also be used as an aid to compassionate listening. Hope can be used as a reminder to be patient and non-judgmental. Hope can also be used to show someone you care.\n\nNote: This conversation is fictional.\n\n",
-                temperature=0.7,
-                max_tokens=256,
-                top_p=1,
-                frequency_penalty=0,
-                presence_penalty=0
-            )
-
-        result = response.get('choices')[0].get('text')
-
-        return {
-            "fulfillmentText":
-            result,
-            "source":
-            "webhookdata"
-        }
-        return '200'
-    except Exception as e:
-        print('error',e)
-        exc_type, exc_obj, exc_tb = sys.exc_info()
-        fname = os.path.split(exc_tb.tb_frame.f_code.co_filename)[1]
-        print('oops',exc_type, fname, exc_tb.tb_lineno)
-        return '400'
-
-
+def generate_answer(question):
+    openai.api_key = "YOUR_OPENAI_API_KEY"
+    prompt = f"Question: {question}\nAnswer:"
+    response = openai.Completion.create(
+        model="davinci:ft-personal-2023-02-24-13-50-55",
+        prompt="This is a discussion between a therapist and a patient",
+        temperature=0.7,
+        max_tokens=1024,
+        n = 1,
+        stop=None,
+        timeout=15,
+        frequency_penalty=0,
+        presence_penalty=0
+    )
+    answer = response.choices[0].text.strip()
+    return answer
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug =True, host='0.0.0.0', port=port)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
